e8eacbb1c4175dc1291d449f8af98083689afab8
Kalaivani Rasalingam <kalaivanirasalingam@Kalaivanis-MacBook-Pro.local>
2018-01-02 23:54:35 -0500

    button class on

---
+++ View/RoundedShadowBtn.swift
@@ -0,0 +1,45 @@
+//
+//  RoundedShadowBtn.swift
+//  iTaxi
+//
+//  Created by Kalaivani Rasalingam on 1/2/18.
+//  Copyright © 2018 Lingeswaran Kandasamy. All rights reserved.
+//
+
+import UIKit
+
+class RoundedShadowBtn: UIButton {
+    func setupView() {
+        self.layer.cornerRadius = 5.0
+        self.layer.shadowRadius = 10.0
+        self.layer.shadowColor = #colorLiteral(red: 0.4078193307, green: 0.4078193307, blue: 0.4078193307, alpha: 1)
+        self.layer.shadowOpacity = 0.3
+        self.layer.shadowOffset = CGSize.zero
+    }
+    func animateButton(shouldLoad: Bool, withMessage message: String?) {
+        let spinner = UIActivityIndicatorView()
+        spinner.activityIndicatorViewStyle = .whiteLarge
+        spinner.color = #colorLiteral(red: 0.4078193307, green: 0.4078193307, blue: 0.4078193307, alpha: 1)
+        spinner.alpha = 0.0
+        spinner.hidesWhenStopped = true
+        if shouldLoad{
+            self.setTitle("", for: .normal)
+            UIView.animate(withDuration: 0.2, animations: {
+                self.layer.cornerRadius = self.frame.height / 2
+                self.frame = CGRect(x: self.frame.midX - (self.frame.height / 2), y: self.frame.origin.y, width: self.frame.height, height: self.frame.height) }, completion: {(finished) in
+                    if finished == true {
+                        self.addSubview(spinner)
+                        spinner.startAnimating()
+                        spinner.center = CGPoint(x: self.frame.width / 2 + 1, y: self.frame.width / 2 + 1)
+                        UIView.animate(withDuration: 0.2, animations: {
+                            spinner.alpha = 1.0
+                        })
+                    }
+                })
+                self.isUserInteractionEnabled = false
+        } else {
+            self.isUserInteractionEnabled = true
+            
+        }
+    }
+}
